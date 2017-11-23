b1d7a1ff8897f780e3828c4f399e9fced4f6f901
Raphael Lima <raphael_lima09@hotmail.com>
2017-11-23 18:42:06 -0200

    Rotina Verifica Existencia Arquivo e Delimitador Pipe
    Verifica se o arquivo existe, se existir remove e alterado o tipo de delimitador do arquivo csv para aceitar filtro de multiplos valores no dashboard.

--- Send_Mail_N_Workbooks_PDF_PNG_CSV.ps1
+++ Send_Mail_N_Workbooks_PDF_PNG_CSV.ps1
@@ -1,79 +1,90 @@
-﻿$csvfile = import-csv -path "uf.csv"
+$CsvFile = import-csv -Path "C:\Script\AutomacaoDisparoEmail\uf.csv" -Delimiter "|"
 
-$server = "http://localhost"
-$username = "stit.santos"
-$password = "Webmotors1"
+$Server = "http://localhost"
+$UserName = "stit.santos"
+$Password = "Webmotors1"
 $Folder = "C:\RelatoriosExportados\IndicadoresWebmotors\"
-$tabcmd = "E:\Program Files\Tableau\Tableau Server\10.3\extras\Command Line Utility\tabcmd.exe"
-$URLPanel = "/views/EstoqueConcorrente-Redshift/"
-$URLWorksheet = New-Object System.Collections.ArrayList
-$URLWorksheet.Add("AvaliaoClassificadosporPortal")
-$URLWorksheet.Add("ClassificadosporPortaleTipoPessoa")
-$URLWorksheet.Add("ClassificadosPorPraa")
-$URLAnalitico = "AnaliticoAbrangnciadeClassificadosPopulao"
+$Tabcmd = "E:\Program Files\Tableau\Tableau Server\10.3\extras\Command Line Utility\tabcmd.exe"
+$UrlPanel = "/views/EstoqueConcorrente-Redshift/"
+$UrlWorksheet = New-Object System.Collections.ArrayList
+$UrlWorksheet.Add("AvaliaoClassificadosporPortal")
+$UrlWorksheet.Add("ClassificadosporPortaleTipoPessoa")
+$UrlWorksheet.Add("ClassificadosPorPraa")
+$UrlAnalitico = "AnaliticoAbrangnciadeClassificadosPopulao"
 $NameExport = "Webmotors_Estoque_Concorrente_"
-$EmailFrom = "stit.santos@webmotors.com.br"
+
+$EmailFrom = "marcus.martins@webmotors.com.br"
 $EmailTo = "stit.santos@webmotors.com.br"
+##"ivan.azevedo@webmotors.com.br"
 $DateToday = (Get-Date).ToString("dd/MM")
 $EmailSubject = "Relatório Estoque Concorrente"
 $EmailBody = "Relatório Estoque Concorrente - $DateToday<BR>"
-$SMTPServer = "smtp.office365.com"
-$SMTPAuthUsername = "stit.santos@webmotors.com.br"
-$SMTPAuthPassword = "Webmotors3"
+$SmtpServer = "smtp.office365.com"
+##"10.11.4.176"
+##"smtp.office365.com"
+$SmtpAuthUsername = "marcus.martins@webmotors.com.br"
+$SmtpAuthPassword = "ALkb7yqawx#"
 
 
-foreach ($line in $csvfile)
+foreach ($Line in $CsvFile)
 {
-  $UF = $line.UF
-  $FileNameCsv = $Folder + $NameExport + $UF + ".csv"
-  $FullURLCsv = $URLPanel + $URLAnalitico + ".csv" + "?:Refresh&DataAtual=Sim&UF=" + $UF
+  $UF = $Line.UF
+  $FileNameCsv = $Folder + $NameExport + ($UF -replace ",", "_") + ".csv"
+  $FullUrlCsv = $UrlPanel + $UrlAnalitico + ".csv" + "?:Refresh&DataAtual=Sim&UF=" + $UF
   $UF
   $FileNameCsv
-  $FullURLCsv
-     & $tabcmd get -s $server -u $username -p $password  $FullURLCsv  -f $FileNameCsv
+  $TesteArquivo = Test-Path $FileNameCsv
+  IF ($TesteArquivo -eq "True")
+      { Remove-Item $FileNameCsv }
+  $FullUrlCsv
+     & $Tabcmd get -s $Server -u $UserName -p $Password  $FullUrlCsv  -f $FileNameCsv
 
-function send_email {
-   $mailmessage = New-Object system.net.mail.mailmessage 
-   $mailmessage.from = ($EmailFrom) 
-   $mailmessage.To.add($EmailTo)
-   $mailmessage.Subject = $EmailSubject + " " + $UF
-   $mailmessage.Body = $EmailBody
-   $attachment1 = New-Object System.Net.Mail.Attachment($FileNameCsv)
-   $mailmessage.Attachments.Add($attachment1)
 
-  foreach ($Element in $URLWorksheet)
+   $MailMessage = New-Object System.Net.Mail.MailMessage 
+   $MailMessage.from = ($EmailFrom) 
+   $MailMessage.To.add($EmailTo)
+   $MailMessage.Subject = $EmailSubject + " " + $UF
+   $MailMessage.Body = $EmailBody
+   $MailMessage.IsBodyHtml = $True
+   $Attachment1 = New-Object System.Net.Mail.Attachment($FileNameCsv)
+   $MailMessage.Attachments.Add($Attachment1)
+
+  foreach ($Element in $UrlWorksheet)
     {
         
-      $FileNamePdf = $Folder + $NameExport + $Element + "_" + $UF + ".pdf"
-      $FileNamePng = $Folder + $NameExport + $Element + "_" + $UF + ".png"
+      $FileNamePdf = $Folder + $NameExport + $Element + "_" + ($UF -replace ",", "_") + ".pdf"
+      $FileNamePng = $Folder + $NameExport + $Element + "_" + ($UF -replace ",", "_") + ".png"
       
-      $FullURLPdf = $URLPanel + $Element + ".pdf" + "?:Refresh&DataAtual=Sim&UF=" + $UF
-      $FullURLPng = $URLPanel + $Element + "?:Refresh&DataAtual=Sim&Size=1600,900&UF=" + $UF
+      $FullUrlPdf = $UrlPanel + $Element + ".pdf" + "?:Refresh&DataAtual=Sim&UF=" + $UF
+      $FullUrlPng = $UrlPanel + $Element + ".png" + "?:Refresh&DataAtual=Sim&Size=1600,900&UF=" + $UF
       
-    $UF
     $Element
     $FileNamePdf
     $FileNamePng
-    $FullURLPdf
-    $FullURLPng
-    print $URLWorksheet[$i]
-      & $tabcmd get -s $server -u $username -p $password  $FullURLPdf  -f $FileNamePdf
-      & $tabcmd get -s $server -u $username -p $password  $FullURLPng  -f $FileNamePng
+    $TesteArquivo = Test-Path $FileNamePdf
+     IF ($TesteArquivo -eq "True")
+         { Remove-Item $FileNamePdf }
+    $TesteArquivo = Test-Path $FileNamePng
+     IF ($TesteArquivo -eq "True")
+         { Remove-Item $FileNamePng }
+    $FullUrlPdf
+    $FullUrlPng
+      & $Tabcmd get -s $Server -u $UserName -p $Password  $FullUrlPdf  -f $FileNamePdf
+      & $Tabcmd get -s $Server -u $UserName -p $Password  $FullUrlPng  -f $FileNamePng
 
-   $attachment2 = New-Object System.Net.Mail.Attachment($FileNamePng)
-   $attachment3 = New-Object System.Net.Mail.Attachment($FileNamePdf)
-   $mailmessage.Attachments.Add($attachment2)
-   $mailmessage.Attachments.Add($attachment3)
+   $Attachment2 = New-Object System.Net.Mail.Attachment($FileNamePng)
+   $Attachment3 = New-Object System.Net.Mail.Attachment($FileNamePdf)
+   $MailMessage.Attachments.Add($Attachment2)
+   $MailMessage.Attachments.Add($Attachment3)
     }  
     
 
-   $mailmessage.IsBodyHTML = $true
-   $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
-   $SMTPClient.Credentials = New-Object System.Net.NetworkCredential("$SMTPAuthUsername", "$SMTPAuthPassword")
-   $SMTPClient.EnableSsl = $true
-   $SMTPClient.Send($mailmessage)
-   }
-send_email    
+
+   $Smtp = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+   $Smtp.Credentials = New-Object System.Net.NetworkCredential("$SmtpAuthUsername", "$SmtpAuthPassword")
+   $Smtp.EnableSsl = $True
+   $Smtp.Send($MailMessage)
+
 
 }
-& $tabcmd logout
+& $Tabcmd logout
